de135e7bd637c3dcb2c05075b08b6debf269e18a
cocositu <kafei1346@outlook.com>
2023-04-06 12:53:07 +0800

    f

---
+++ build/src/CMakeFiles/MyProject.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Project/NUS_OSTC/BuildMaze_PrimeAlgorithm/src/maze.cpp" "src/CMakeFiles/MyProject.dir/maze.cpp.obj" "gcc" "src/CMakeFiles/MyProject.dir/maze.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/CMakeFiles/MyProject.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyProject.dir/maze.cpp.obj"
+  "CMakeFiles/MyProject.dir/maze.cpp.obj.d"
+  "MyProject.exe"
+  "MyProject.exe.manifest"
+  "MyProject.pdb"
+  "libMyProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
